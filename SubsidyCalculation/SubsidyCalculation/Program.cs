b96f32d2f713fdb46f4a697cca8689d9f398948a
aliardan <33637478+aliardan@users.noreply.github.com>
2021-04-05 00:33:17 +0300

    Invoke implementation

--- SubsidyCalculation/SubsidyCalculation/Program.cs
+++ SubsidyCalculation/SubsidyCalculation/Program.cs
@@ -6,7 +6,30 @@ namespace SubsidyCalculation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tariff tariff = new Tariff() { ServiceId = 1, HouseId = 1, PeriodBegin = DateTime.Today, PeriodEnd = DateTime.UtcNow, Value = 10 };
+            Volume volume = new Volume() { ServiceId = 1, HouseId = 1, Month = DateTime.UtcNow, Value = 10 };
+            SubsidyCalculation subsidyCalculation = new SubsidyCalculation();
+            subsidyCalculation.OnNotify += Notify;
+            subsidyCalculation.OnException += Exception;
+            Charge charge = subsidyCalculation.CalculateSubsidy(volume, tariff);
+
+            if (charge != null)
+                Console.WriteLine($"Расчет: ServiceId - {charge.ServiceId}, " +
+                                  $"HouseId - {charge.HouseId}, " +
+                                  $"Month - {charge.Month}, " +
+                                  $"Value - {charge.Value}");
+            else
+                Console.WriteLine("Ошибка");
+
+        }
+
+        private static void Notify(object? sender, string e)
+        {
+            Console.WriteLine(e);
+        }
+        private static void Exception(object? sender, Tuple<string, Exception> e)
+        {
+            Console.WriteLine(e.Item2);
         }
     }
 }
